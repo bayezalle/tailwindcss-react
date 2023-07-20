e8e99ae7c18409b2fe77decdd3609c8c4a7ddb38
saliou thioune <bayezalle995@gmail.com>
2023-07-20 14:01:15 +0000

    nouvea ajout

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
-const CTA = () => {
-  return (
+const CTA = () =>  (
+   <section className={`${styles.flexCenter} ${styles.marginY}
+   ${styles.padding} sm:flex-row flex-col bg-black-gradient-2
+   rounded-2[20px] box-shadow`}>
     <div>
-      
+      <h2>Let's try our service now</h2>
+      <p>Everything you need to accept card payements
+        and grow your business anywhre on the planet.
+
+      </p>
     </div>
+    <div>
+      <Button />
+    </div>
+   </section> 
   )
-}
 
 export default CTA
